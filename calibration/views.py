1e42a8980734e64611a4f616b37b48a08f547e04
Edcarlos Neves <edcarlosp@gmail.com>
2020-11-05 22:14:12 -0300

    Não permitir análises com nomes duplicados

--- calibration/views.py
+++ calibration/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from django.views import generic
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
@@ -11,15 +12,16 @@ from calibration.forms import AnalysisForm
 @login_required
 def index(request):
     form = AnalysisForm(request.POST or None)
+    context = {
+        "form": form,
+    }
     if request.method == "POST":
         form = AnalysisForm(request.POST or None)
-
         if form.is_valid():
-            form.save()
-            obj = Analysis.objects.latest("id")
-            print("print(obj)")
-            print(obj)
             analysis_name = form.cleaned_data["analysis_name"]
+            obj = Analysis.objects.filter(analysis_name=analysis_name)
+            if not obj:
+                form.save()
             substance_name = form.cleaned_data["substance_name"]
             absorbance = [
                 float(data) for data in form.cleaned_data["absorbance"].split(",")
@@ -34,10 +36,6 @@ def index(request):
                 "substance_name": substance_name,
             }
             return render(request, "report.html", context=context)
-    context = {
-        "form": form,
-    }
-
     return render(request, "index.html", context=context)
 
 
